94f783e705bf515f70f33bbd7ffa68b9d21cdbb6
Juan Francisco Fioretti <juanfrafio125@gmail.com>
2022-06-21 11:31:26 -0300

    primer commit del proyecto de psicología

---
+++ enviar.php
@@ -0,0 +1,44 @@
+<?php
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$mail = $_POST['mail'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su mail es: " . $mail . " \r\n";
+$message .= "Su teléfono es: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'juanfrafio125@gmail.com';
+$asunto = 'Asunto del mensaje';
+
+mail($para, $asunto, $message, $header);
+
+header('Location:contacto.html');
+?>
+
+<?php
+$message = $_POST['message'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'juanfrafio125@gmail.com';
+$asunto = 'Asunto del mensaje';
+
+mail($para, $asunto, $message, $header);
+
+header('Location:nosotros.html');
+?>
